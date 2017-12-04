0ac26b8e1b1dc3517bfb4552c8ce85ab6cb006c0
lj1035 <lj1035@nyu.edu>
2017-12-04 14:33:44 -0500

    Minor fix

--- decomp_att/data_loader.py
+++ decomp_att/data_loader.py
@@ -80,12 +80,4 @@ def batch_iter(dataset, batch_size, shuffle=True):
             item.extend([100] * (max_length - len(item)))
         for item in hypothesis:
             item.extend([100] * (max_length - len(item)))
-        yield [label, premise, hypothesis]
-
-
-if __name__ == '__main__':
-    vocab, word_embeddings, word_to_index, index_to_word = load_embedding_and_build_vocab('../data/glove.6B.300d.txt')    
-    data = process_snli('../data/snli_1.0_dev.jsonl', word_to_index, to_lower=True)
-    print(data[1])
-    dev_iter = batch_iter(dataset=data, batch_size=2, shuffle=True)
-    print(next(dev_iter)) 
+        yield [label, premise, hypothesis] 
