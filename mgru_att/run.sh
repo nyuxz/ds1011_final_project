a85ab0e64a85c404209dbaa82012deef18005f6b
nyuxz <xz1757@nyu.edu>
2017-12-05 16:18:32 -0500

    Update run.sh

--- mgru_att/run.sh
+++ mgru_att/run.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --time=40:00:00
 #SBATCH --mem=10GB
-#SBATCH --job-name=mgru_att
+#SBATCH --job-name=mgru_att_wbw
 #SBATCH --mail-type=END
 #SBATCH --mail-user= email
 #SBATCH --output=slurm_%j.out
@@ -15,4 +15,4 @@ module load pytorch/python3.5/0.2.0_3
 module load cuda/8.0.44
 module load cudnn/8.0v5.1
 
-time python3 mgru_att.py 
+time python3 mgru_att_wbw.py 
